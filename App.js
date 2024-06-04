2a2cd92dd92fb9a44190af9f95a0910f88a3793d
Vyas, Devesh <devesh.vyas@intel.com>
2024-06-04 21:37:35 +0530

    replaced React.createElement into JSX syntax

--- App.js
+++ App.js
@@ -1,58 +1,9 @@
-/**
- * <div id="parent">
- *      <div id="child">
- *            <h1>I am an H1 Tag!</h1>
- *            <h2>I am an H2 Tag!</h2>
- *      </div>
- *      <div id="child2">
- *            <h1>I am an H1 Tag!</h1>
- *            <h2>I am an H2 Tag!</h2>
- *      </div>
- * </div>
- */
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" }, [
-    React.createElement(
-        "div",
-        { id: "child" },
-        [
-            React.createElement(
-                "h1",
-                { "id": "h1" },
-                "I'm an H1 Tag!"
-            ),
-            React.createElement(
-                "h2",
-                { "id": "h2" },
-                "I'm an H2 Tag!"
-            )
-        ]
-    ),
-    React.createElement(
-        "div",
-        { id: "child2" },
-        [
-            React.createElement(
-                "h1",
-                { "id": "h1" },
-                "I'm an H1 Tag!"
-            ),
-            React.createElement(
-                "h2",
-                { "id": "h2" },
-                "I'm an H2 Tag!"
-            )
-        ]
-    )
-]
-)
+const jsxHeading = <h1 id="heading">I am H1 heading using JSX</h1>;
 
-console.log(parent);
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(jsxHeading);
